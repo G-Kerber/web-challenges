8d9217c4d9aa94f408476bf29c13f2eb2efe70c4
Herr <g.kerber@posteo.it>
2024-12-05 15:59:11 +0100

    add solution for challenge js wheather

--- 14_JS-conditions/js-conditions-and-booleans_weather-check/js/index.js
+++ 14_JS-conditions/js-conditions-and-booleans_weather-check/js/index.js
@@ -1,5 +1,30 @@
 // Change this value to test different "weather" conditions.
-const weather = "rainy"
+const weather = "rainy";
 
 // Adjust the "temperature" value to trigger different messages (for Part 2 only).
-const temperature = 22; 
+const temperature = 22;
+
+switch (weather) {
+  case "sunny":
+    if (temperature > 20) {
+      console.log("It's sunny. Think an your sunscreen cream!");
+    } else {
+      console.count("It's sunny, but a bit chilly!");
+    }
+    break;
+  case "rainy":
+    console.log("It's raining, don't forget your umbrella!");
+    break;
+  case "snowy":
+    if (temperature > 0) {
+      console.log("It's snowing, stay warm!");
+    } else {
+      console.log("Freezing snow!");
+    }
+    break;
+  case "windy":
+    console.log("It's windy. Your need a kite.");
+    break;
+  default:
+    console.log("I'm not sure what the weather is like.");
+}
