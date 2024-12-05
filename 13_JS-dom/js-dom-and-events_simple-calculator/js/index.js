b0bbf9b7108bc88bf99274b23ddf5fb3ffa4d429
Herr <g.kerber@posteo.it>
2024-12-05 11:23:28 +0100

    add calculation for life cycle

--- 13_JS-dom/js-dom-and-events_simple-calculator/js/index.js
+++ 13_JS-dom/js-dom-and-events_simple-calculator/js/index.js
@@ -1,6 +1,6 @@
 console.clear();
 
-const operand1 = 12;
+let operand1 = 12;
 const operand2 = 4;
 
 // ----- Mathematical Operations -----
@@ -8,7 +8,12 @@ const operand2 = 4;
 // Step 1: Use `document.querySelector` to select each button by its `data-js` attribute.
 
 // --v-- write your code here --v--
-
+const buttonAdd = document.querySelector('[data-js="add"]');
+const buttonSub = document.querySelector('[data-js="subtract"]');
+const buttonMulti = document.querySelector('[data-js="multiply"]');
+const buttonDiv = document.querySelector('[data-js="divide"]');
+const buttonExp = document.querySelector('[data-js="exponent"]');
+const buttonMod = document.querySelector('[data-js="modulo"]');
 // --^-- write your code here --^--
 
 /* 
@@ -22,7 +27,25 @@ For each operation (add, subtract, multiply, divide, exponent, and modulo):
 */
 
 // --v-- write your code here --v--
-
+let result;
+buttonAdd.addEventListener("click", () => {
+  console.log((result = operand1 + operand2));
+});
+buttonSub.addEventListener("click", () => {
+  console.log((result = operand1 - operand2));
+});
+buttonMulti.addEventListener("click", () => {
+  console.log((result = operand1 * operand2));
+});
+buttonDiv.addEventListener("click", () => {
+  console.log((result = operand1 / operand2));
+});
+buttonExp.addEventListener("click", () => {
+  console.log((result = operand1 ** operand2));
+});
+buttonMod.addEventListener("click", () => {
+  console.log((result = operand1 % operand2));
+});
 // --^-- write your code here --^--
 
 // ----- Update the First Operand -----
@@ -38,5 +61,40 @@ Step 2: Add event listeners to update `operand1` based on the button clicked. Lo
 */
 
 // --v-- write your code here --v--
+const buttonAdd1 = document.querySelector('[data-js="increase-by-one"]');
+const buttonAdd5 = document.querySelector('[data-js="increase-by-five"]');
+const buttonSub1 = document.querySelector('[data-js="decrease-by-one"]');
+const buttonSub5 = document.querySelector('[data-js="decrease-by-five"]');
+const buttonMulti2 = document.querySelector('[data-js="multiply-by-two"]');
+const buttonDiv2 = document.querySelector('[data-js="divide-by-two"]');
 
+buttonAdd1.addEventListener("click", () => {
+  result = operand1 += 1;
+  console.log(result);
+});
+
+buttonAdd5.addEventListener("click", () => {
+  result = operand1 += 5;
+  console.log(result);
+});
+
+buttonSub1.addEventListener("click", () => {
+  result = operand1--; //Es wird zuerst immer der zuletzt geklickte Befehl ausgeführt bevor der Wert um 1 reduziert wird. Nur im Fall der Kurzschreibweise -- / ++
+  console.log(result);
+});
+
+buttonSub5.addEventListener("click", () => {
+  result = operand1 -= 5;
+  console.log(result);
+});
+
+buttonMulti2.addEventListener("click", () => {
+  result = operand1 *= 2;
+  console.log(result);
+});
+
+buttonDiv2.addEventListener("click", () => {
+  result = operand1 /= 2;
+  console.log(result);
+});
 // --^-- write your code here --^--
