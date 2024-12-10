aa9f371bdbdd6c0b64eced51d963905cb92bf317
Herr <g.kerber@posteo.it>
2024-12-10 16:00:12 +0100

    solve challenge Toast Messages

--- 20_js-createElement/js-createelement_toast-messages/js/index.js
+++ 20_js-createElement/js-createelement_toast-messages/js/index.js
@@ -4,10 +4,20 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", () => {
+addButton.addEventListener("click", (event) => {
   // Exercise: Append a new entry to the toast messages container
+  event.preventDefault();
+
+  const newToast = document.createElement("li");
+  toastContainer.append(newToast);
+  newToast.classList.add("toast-container__message");
+  newToast.textContent = "another toast";
 });
 
-clearButton.addEventListener("click", () => {
+clearButton.addEventListener("click", (event) => {
   // Exercise: Clear the stack of toast messages
+  event.preventDefault();
+  toastContainer.remove();
+  toastContainer.reset;
+  location.reload();
 });
