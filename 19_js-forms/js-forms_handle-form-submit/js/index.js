75682ae3914e409a8a7e72311939fb39e23d11a2
Herr <g.kerber@posteo.it>
2024-12-10 11:45:30 +0100

    solve Part 1: Handle Form Submit

--- 19_js-forms/js-forms_handle-form-submit/js/index.js
+++ 19_js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,23 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log("Object_", data);
+
+  const ageBadnessSum = event.target.badness.value + event.target.age.value;
+  console.log(
+    "The age-badness-sum of " +
+      event.target.firstName.value +
+      " is " +
+      ageBadnessSum
+  );
+
+  event.target.reset();
+  event.target.firstName.focus();
+});
