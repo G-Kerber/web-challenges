7019a478d33554724757ab7ba17e22b617cf5c93
Herr <g.kerber@posteo.it>
2024-12-10 17:21:54 +0100

    solve challenge Social Media Post

--- 20_js-createElement/js-createelement_social-media-post/js/index.js
+++ 20_js-createElement/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,35 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+// Define variables for HTML elements
+const newPost = document.createElement("article");
+const newPostText = document.createElement("p");
+const newPostFooter = document.createElement("footer");
+const newPostUsername = document.createElement("span");
+const newPostButton = document.createElement("button");
+
+// add new Post at the body
+document.body.append(newPost);
+newPost.classList.add("post");
+
+// define new post
+newPost.append(newPostText);
+newPostText.classList.add("post__content");
+newPostText.textContent =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+
+newPost.append(newPostFooter);
+newPostFooter.classList.add("post__footer");
+
+newPostFooter.append(newPostUsername);
+newPostUsername.classList.add("post__username");
+newPostUsername.textContent = "@username";
+
+newPostFooter.append(newPostButton);
+newPostButton.classList.add("post__button");
+newPostButton.setAttribute("type", "button");
+newPostButton.setAttribute("data-js", "like-Button");
+newPostButton.textContent = "♥ Like";
+
+newPostButton.addEventListener("click", handleLikeButtonClick);
