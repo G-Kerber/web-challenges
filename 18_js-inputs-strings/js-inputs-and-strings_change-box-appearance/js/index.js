d92362e21f90c4e3d4b7abe399c172b08c708460
Herr <g.kerber@posteo.it>
2024-12-11 10:06:48 +0100

    saving interism result by switch branch

--- 18_js-inputs-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ 18_js-inputs-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -6,7 +6,11 @@ const radiusInput = document.querySelector('[data-js="input-radius"]');
 const rotationInput = document.querySelector('[data-js="input-rotation"]');
 
 colorInput.addEventListener("input", (event) => {
-  box.background = event.target.value;
+  const tempVar = event.target.value;
+  box.background1 = "hsl(" + tempVar + ", 70%, 60%)";
+  box.background = "hsl(${tempVar}, 70%, 60%)";
+  console.log(box.background);
 });
+
 radiusInput.addEventListener("", () => {});
 rotationInput.addEventListener("", () => {});
