679cb3d12dbb1393ed413a64699636606037f1dd
Herr <g.kerber@posteo.it>
2024-12-16 11:43:33 +0100

    solve first task

--- 23_js-structure/js-structure_shapes/index.js
+++ 23_js-structure/js-structure_shapes/index.js
@@ -1,23 +1,28 @@
 console.clear();
 
+import { getRandomColor } from "./utils/randomColor.js";
+
 const root = document.getElementById("root");
 
 const circle = document.createElement("div");
 circle.classList.add("circle");
 circle.addEventListener("click", () => {
-  circle.style.backgroundColor = "#ccc";
+  const randomColor = getRandomColor();
+  circle.style.backgroundColor = randomColor;
 });
 
 const square = document.createElement("div");
 square.classList.add("square");
 square.addEventListener("click", () => {
-  square.style.backgroundColor = "#ccc";
+  const randomColor = getRandomColor();
+  square.style.backgroundColor = randomColor;
 });
 
 const pentagon = document.createElement("div");
 pentagon.classList.add("pentagon");
 pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = "#ccc";
+  const randomColor = getRandomColor();
+  pentagon.style.backgroundColor = randomColor;
 });
 
 root.append(circle, square, pentagon);
