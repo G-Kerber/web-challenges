adc3d9a89eaae40b3058e63a0feb43830026c4c5
Herr <g.kerber@posteo.it>
2024-12-16 21:25:16 +0100

    solve Color Clue Game

--- 24_js-fetch/js-fetch_color-clue/js/fetch.js
+++ 24_js-fetch/js-fetch_color-clue/js/fetch.js
@@ -18,6 +18,10 @@ export async function fetchNewColor() {
    */
 
   // --v-- your code here --v--
-
+  const response = await fetch(colorApiUrl);
+  const data = await response.json();
+  const ColorHex = data.name.closest_named_hex;
+  const ColorName = data.name.value;
+  setColorToGuess(ColorHex, ColorName);
   // --^-- your code here --^--
 }
