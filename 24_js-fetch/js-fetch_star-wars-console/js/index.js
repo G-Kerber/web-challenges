2c4b44e0b72d164ecec6d77136c7cacd5ad52582
Herr <g.kerber@posteo.it>
2024-12-16 17:37:37 +0100

    solve real Star Wars API - Console

--- 24_js-fetch/js-fetch_star-wars-console/js/index.js
+++ 24_js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,16 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log("entire data object_", data);
+  console.log("R2-D2_", data.results[2]);
+  const name = "R2-D2";
+  const parttimeVar = data.results.find((character) => {
+    return character.name === name;
+  });
+  console.log(parttimeVar.eye_color);
+}
 
 fetchData();
