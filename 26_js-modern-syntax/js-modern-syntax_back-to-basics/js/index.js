8958b7e5f05175fadc9e128e00f8e7986502ff63
Herr <g.kerber@posteo.it>
2024-12-19 23:43:53 +0100

    resolved Bonus: Back to Basics

--- 26_js-modern-syntax/js-modern-syntax_back-to-basics/js/index.js
+++ 26_js-modern-syntax/js-modern-syntax_back-to-basics/js/index.js
@@ -3,15 +3,29 @@ Now that you've practiced destructuring, default parameters, and the spread oper
 try reversing the challenge by rewriting this code without using these modern features.
 */
 
-export const getNameAndCountry = ({ name, country }) => [name, country];
+//export const getNameAndCountry = ({ name, country }) => [name, country];
+export function getNameAndCountry(city) {
+  return [city.name, city.country];
+}
 
-export const getRelocatedCity = (
-  city1,
-  city2 = { name: "Berlin", country: "Germany" }
-) => {
-  const [, country] = getNameAndCountry(city2);
-  return {
-    ...city1,
-    country,
-  };
-};
+//export const getRelocatedCity = (
+//  city1,
+//  city2 = { name: "Berlin", country: "Germany" }
+//) =>
+export function getRelocatedCity(city1, city2) {
+  //const [, country] = getNameAndCountry(city2);
+  const country = getNameAndCountry(city2)[1];
+
+  //return {
+  //  ...city1,
+  //  country,
+  //};
+  const city = getNameAndCountry(city1)[0];
+  const relocatedCity = { city, country };
+  return relocatedCity;
+}
+
+const city3 = { name: "Wiesbaden", country: "Hessen" };
+const city4 = { name: "Stuttgart", country: "BW" };
+const cityIsRelocated = getRelocatedCity(city3, city4);
+console.log(cityIsRelocated);
