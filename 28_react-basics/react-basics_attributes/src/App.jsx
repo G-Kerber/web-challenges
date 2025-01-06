337bc8947ff71fa126f27d3c82cb1e086713e70d
Herr <g.kerber@posteo.it>
2025-01-06 13:11:08 +0100

    resolve challenge react atrributes

--- 28_react-basics/react-basics_attributes/src/App.jsx
+++ 28_react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,22 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return article();
+}
+
+function article() {
+  return (
+    <article className="article">
+      Replace me with an article
+      <h2 className="article__title">something like a title</h2>
+      <label htmlFor="color">Enter your favorite color:</label>
+      <input type="text" id="color" name="color" required minLength="3"></input>
+      <a
+        className="article__link"
+        href="https://de.wikipedia.org/wiki/Model_View_Controller"
+      >
+        Model View Controller
+      </a>
+    </article>
+  );
 }
