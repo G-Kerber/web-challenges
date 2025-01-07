b1d163bb6530dc3fb5f2178568cfd60b6ff3b2e1
Herr <g.kerber@posteo.it>
2025-01-07 21:37:36 +0100

    refactoring solution for challenge react state 1 box

--- 31_react-state-1/react-state-1_box/src/App.jsx
+++ 31_react-state-1/react-state-1_box/src/App.jsx
@@ -3,19 +3,19 @@ import "./styles.css";
 
 export default function App() {
   const [isActive, setIsActive] = useState(false);
-  const [buttonLabel, setButtonLabel] = useState("Activate");
 
   function handleClick() {
     setIsActive(!isActive);
     // Check that the value changes correctly.
     console.log(isActive);
-    setButtonLabel(buttonLabel === "Activate" ? "Deactivate" : "Activate");
   }
 
   return (
     <main>
       <div className={`box ${isActive ? "box--active" : ""}`} />
-      <button onClick={handleClick}>{buttonLabel}</button>
+      <button onClick={handleClick}>
+        {isActive ? "Deactivate" : "Activate"}
+      </button>
     </main>
   );
 }
