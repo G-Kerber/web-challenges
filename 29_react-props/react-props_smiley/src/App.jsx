6c02118dc5f807ff938ec0432d47fcac236e36c7
Herr <g.kerber@posteo.it>
2025-01-07 11:32:25 +0100

    add new challenge: react nesting

--- 29_react-props/react-props_smiley/src/App.jsx
+++ 29_react-props/react-props_smiley/src/App.jsx
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Smiley isHappy />;
+}
+
+function Smiley({ isHappy }) {
+  return (
+    <div>
+      <h1>react props - challenge Smiley</h1>
+      <p>{isHappy ? "😀" : "😒"}</p>
+    </div>
+  );
 }
