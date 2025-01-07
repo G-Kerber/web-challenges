c5e3bbd7129610e26809a4344764994375217024
Herr <g.kerber@posteo.it>
2025-01-07 11:43:02 +0100

    resolve challenge react-props-smiley

--- 29_react-props/react-props_smiley/src/App.jsx
+++ 29_react-props/react-props_smiley/src/App.jsx
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Smiley isHungry />;
+}
+
+function Smiley({ isHungry }) {
+  return (
+    <div>
+      <h1>Challenge: react props - smiley</h1>
+      <p>{isHungry ? "🙂" : "😒"}</p>
+    </div>
+  );
 }
