309e4f82b726773e907bef70614d9d4f78af52f3
Herr <g.kerber@posteo.it>
2025-01-07 09:39:09 +0100

    resolve challenge react-props_sum

--- 29_react-props/react-props_sum/src/App.jsx
+++ 29_react-props/react-props_sum/src/App.jsx
@@ -2,5 +2,17 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Sum valueA="1" valueB="2" />;
+}
+
+function Sum({ valueA, valueB }) {
+  let sum = Number(valueA) + Number(valueB);
+  return (
+    <div>
+      <h1>Challenge - react-props_sum</h1>
+      <p>
+        {sum} ist die Summe aus {valueA} und {valueB}
+      </p>
+    </div>
+  );
 }
