9a1b6ebf73f04fe6368d449f728b8ee430f0d967
Herr <g.kerber@posteo.it>
2025-01-07 16:08:24 +0100

    resolve first challenge react state 1 box

--- 31_react-state-1/react-state-1_box/src/App.jsx
+++ 31_react-state-1/react-state-1_box/src/App.jsx
@@ -1,18 +1,21 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  let isActive = false;
+  const [isActive, setIsActive] = useState(false);
+  const [buttonLabel, setButtonLabel] = useState("Activate");
 
   function handleClick() {
-    isActive = !isActive;
+    setIsActive(!isActive);
     // Check that the value changes correctly.
     console.log(isActive);
+    setButtonLabel(buttonLabel === "Activate" ? "Deactivate" : "Activate");
   }
 
   return (
     <main>
       <div className={`box ${isActive ? "box--active" : ""}`} />
-      <button onClick={handleClick}>Activate</button>
+      <button onClick={handleClick}>{buttonLabel}</button>
     </main>
   );
 }
