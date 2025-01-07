d1d43f431258f5daf928be4c549877895e71cf45
Herr <g.kerber@posteo.it>
2025-01-07 15:36:50 +0100

    interism result challenge reaxt nesting navigation

--- 30_react-nesting/react-nesting_navigation/src/App.jsx
+++ 30_react-nesting/react-nesting_navigation/src/App.jsx
@@ -1,4 +1,6 @@
 import "./styles.css";
+import Header from "./components/header.jsx";
+import Navigation from "./components/navigation.jsx";
 
 import avatar from "./img/avatar.jpg";
 import logo from "./img/logo.jpg";
@@ -6,11 +8,11 @@ import logo from "./img/logo.jpg";
 export default function App() {
   return (
     <>
-      <header className="header">
+      <Header>
         <a href="#">
           <img className="round-image" src={logo} alt="logo" />
         </a>
-        <nav>
+        <Navigation>
           <a className="navigation__link" href="#home">
             Home
           </a>
@@ -20,7 +22,7 @@ export default function App() {
           <a className="navigation__link" href="#impressum">
             Impressum
           </a>
-        </nav>
+        </Navigation>
         <button
           type="button"
           onClick={() => console.log("I could toggle a profile!")}
@@ -28,7 +30,7 @@ export default function App() {
         >
           <img className="round-image" src={avatar} alt="avatar" />
         </button>
-      </header>
+      </Header>
       <main>content goes here…</main>
     </>
   );
