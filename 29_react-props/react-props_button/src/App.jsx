f6ebd1323dfa95a15a5733eb9d7b80e732c4d1f6
Herr <g.kerber@posteo.it>
2025-01-08 10:35:22 +0100

    saving interim result before switch branch

--- 29_react-props/react-props_button/src/App.jsx
+++ 29_react-props/react-props_button/src/App.jsx
@@ -2,5 +2,17 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <>
+      <h1>Challenge react props - button</h1>
+      <Button color="#Ff0000" text="Label" disabled={true} />
+    </>
+  );
+}
+function Button({ color, text, disabled }) {
+  return (
+    <button type="button" disabled={disabled} style={{ color: { color }, backgroundColor: #006400 }}>
+      {text}
+    </button>
+  );
 }
