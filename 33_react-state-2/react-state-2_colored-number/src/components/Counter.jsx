ecbbdaad2b09c270e8fbb31f11f454747b2460f0
Herr <g.kerber@posteo.it>
2025-01-08 16:07:20 +0100

    resolve challenge react-state-2 color

--- 33_react-state-2/react-state-2_colored-number/src/components/Counter.jsx
+++ 33_react-state-2/react-state-2_colored-number/src/components/Counter.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import ColoredNumber from "./ColoredNumber";
 
 export default function Counter() {
+  let [value, setValue] = useState(50);
   return (
     <div className="counter">
       <h1>Colored Number</h1>
-      <ColoredNumber value={0} />
+      <ColoredNumber value={value} />
       <div className="counter__buttons-container">
         <button
           type="button"
           className="counter__button"
           aria-label="increment count"
+          onClick={() => setValue(value + 1)}
         >
           +
         </button>
@@ -17,6 +20,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement count"
+          onClick={() => setValue(value - 1)}
         >
           -
         </button>
