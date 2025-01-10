4e587a296e8f7f8349bd256822f49910b0026a32
Herr <g.kerber@posteo.it>
2025-01-10 12:50:15 +0100

    pimp challenge react-fetch_pokeomon-api-paging with variable limit

--- 35_react-effect-fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
+++ 35_react-effect-fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
@@ -5,12 +5,13 @@ export default function PokemonList() {
   const [offset, setOffset] = useState(0);
   const [pagePrev, setPagePrev] = useState();
   const [pageNext, setPageNext] = useState();
+  const limit = 20;
 
   useEffect(() => {
     async function loadPokemon() {
       try {
         const response = await fetch(
-          `https://pokeapi.co/api/v2/pokemon?offset=${offset}`
+          `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
         );
         const data = await response.json();
         setPokemon(data.results);
@@ -28,14 +29,14 @@ export default function PokemonList() {
     <main>
       <button
         type="button"
-        onClick={() => setOffset(offset - 20)}
+        onClick={() => setOffset(offset - limit)}
         disabled={pagePrev === null ? true : false}
       >
         Previous Page
       </button>
       <button
         type="button"
-        onClick={() => setOffset(offset + 20)}
+        onClick={() => setOffset(offset + limit)}
         disabled={pageNext === null ? true : false}
       >
         Next Page
