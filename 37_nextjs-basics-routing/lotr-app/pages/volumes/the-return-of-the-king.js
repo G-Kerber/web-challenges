177b3515e356a414ebedc8f265ecbb7fcb251c96
Herr <g.kerber@posteo.it>
2025-01-16 15:29:35 +0100

    resolve challenge nextjs basics

---
+++ 37_nextjs-basics-routing/lotr-app/pages/volumes/the-return-of-the-king.js
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import { volumes } from "../../lib/data";
+import Image from "next/image";
+
+export default function theFellowshipOfTheRing() {
+  const bookTitle = "the-return-of-the-king";
+  const volumesIndex = volumes.findIndex(({ slug }) => slug === bookTitle);
+  const volume = volumes.find(({ slug }) => slug === bookTitle);
+  const { title, description, books, cover } = volume;
+  const volumesLength = Object.values(volumes).length;
+
+  return (
+    <>
+      <p>
+        <Link href={`..`}>Home Page</Link>
+      </p>
+      <p>
+        <Link href={`../volumes`}>Lord of the Rings</Link>
+      </p>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <ul>
+        {books.map((book) => {
+          return (
+            <li>
+              {book.ordinal}: {book.title}
+            </li>
+          );
+        })}
+      </ul>
+      <Image alt="cover" width={140} height={230} src={`/../public${cover}`} />
+      <p>
+        {volumesIndex > 0 && (
+          <Link href={`/volumes/${volumes[volumesIndex - 1].slug}`}>
+            previous book
+          </Link>
+        )}
+        {volumesIndex < volumesLength - 1 && (
+          <Link href={`/volumes/${volumes[volumesIndex + 1].slug}`}>
+            next book
+          </Link>
+        )}
+      </p>
+    </>
+  );
+}
