543d153f392bc17d0a5c4577cb86bb710cfdc870
Herr <g.kerber@posteo.it>
2025-01-21 15:32:51 +0100

    saving interism result before branch chnage

--- 42_react_global-state/react-global-state_lights/components/Light/index.js
+++ 42_react_global-state/react-global-state_lights/components/Light/index.js
@@ -1,22 +1,15 @@
-import { useState } from "react";
 import { LightButton, Icon, Text, Name, State } from "./Light.styled";
 
-export default function Light({ name }) {
-  const [isOn, setIsOn] = useState(false);
-
-  function handleToggle() {
-    setIsOn((isOn) => !isOn);
-  }
-
+export default function Light({ id, name, isOn, toggleLight }) {
   return (
     <LightButton
       type="button"
       onClick={() => {
-        handleToggle();
+        toggleLight(id);
       }}
       $isOn={isOn}
     >
-      <Icon $isOn={isOn}>💡</Icon>
+      <Icon isOn={isOn}>💡</Icon>
       <Text>
         <Name>{name}</Name>
         <State>{isOn ? "On" : "Off"}</State>
