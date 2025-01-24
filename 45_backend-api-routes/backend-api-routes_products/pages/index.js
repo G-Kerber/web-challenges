0bd96a783cd7da81d5694096e7583aaee2443c04
Herr <g.kerber@posteo.it>
2025-01-24 16:57:06 +0100

    resolve challenge backend-api-routes

--- 45_backend-api-routes/backend-api-routes_products/pages/index.js
+++ 45_backend-api-routes/backend-api-routes_products/pages/index.js
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
     <div>
       <h1>Welcome to Next.js API Routes!</h1>
+      <Link href={`/api/products`}>/api/products</Link>
+      <Link href={`/api/products/1`}>/api/products/1</Link>
+      <Link href={`/api/products/2`}>/api/products/2</Link>
+      <Link href={`/products`}>/products</Link>
+      <Link href={`/products/1`}>/products/1</Link>
+      <Link href={`/products/2`}>/products/2</Link>
     </div>
   );
 }
