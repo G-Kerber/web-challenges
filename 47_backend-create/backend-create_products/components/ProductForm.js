e6982110394660c294dda95e151dcc228efa0114
Herr <g.kerber@posteo.it>
2025-01-28 11:58:47 +0100

    resolve backend-creapte-products

--- 47_backend-create/backend-create_products/components/ProductForm.js
+++ 47_backend-create/backend-create_products/components/ProductForm.js
@@ -1,12 +1,27 @@
 import styled from "styled-components";
 import StyledButton from "@/components/Button";
+import useSWR from "swr";
 
 export default function ProductForm() {
+  const URL = "/api/products";
+  const { mutate } = useSWR(URL);
+
   async function handleSubmit(event) {
     event.preventDefault();
 
     const formData = new FormData(event.target);
     const productData = Object.fromEntries(formData);
+
+    const response = await fetch(URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+      console.log(response);
+    }
   }
 
   return (
